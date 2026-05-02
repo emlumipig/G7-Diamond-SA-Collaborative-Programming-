d61d825527e744df7105add5c2de5f603556c56e
Nor <ntlumontod@cmc.pshs.edu.ph>
2026-05-02 18:04:20 +0800

    main.py

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ def compute_grades(TQ1, TQ2, TQ3, TQ4):
     Q3 = (Q2 + 2 * TQ3) / 3
     Q4 = (Q3 + 2 * TQ4) / 3
     return Q1, Q2, Q3, Q4
+    
+def adjectival_grade(grade):
+    if grade >= 90:
+        return "Outstanding"
+    elif grade >= 85:
+        return "Very Satisfactory"
+    elif grade >= 80:
+        return "Satisfactory"
+    elif grade >= 75:
+        return "Fairly Satisfactory"
+    else:
+        return "Did not meet Expectations"
+    
